b7b7bf69f16f694f9e0c9e95578cf44952d39415
zhangr16 <zhangr16@uni.coventry.ac.uk>
2016-03-01 16:03:52 +0000

    Create Syntax.js

---
+++ Syntax.js
@@ -0,0 +1,9 @@
+1.seperated by semocolons. /*分号*/
+
+2.string written with quotes. /*单双引号都行*/
+
+3.Code after  is treated as a comment.  /**/或者//
+
+4.case-sensiteive /*区分大小写*/
+
+5.starts with a lowercase letter: firstName, lastName, masterCard, interCity /*小写开头*/
