d0ad8a32d434b21b975db2c913d3aa94beb74522
zhangr16 <zhangr16@uni.coventry.ac.uk>
2016-03-08 23:38:11 +0000

    Update Datatype.js

--- Datatype.js
+++ Datatype.js
@@ -1,1 +1,19 @@
-# java-script-tutorial
+1.数据类型：
+
+(1)Number:   var length = 16;  数字
+
+(2)String:   var lastName = "Andy";  字符串
+
+(3)Array:    var cars = ["Saad", "Volvo", "BMW"];  数组
+
+(4)Object:   var x = {firstName: "Andy", lastName: "Doe"};   对象,客体
+
+(5)Boolean:  var x = true; var y = false;   正误
+
+
+
+2.eg:
+
+(1)var x = "a" + 11 + 2;    //a112
+
+(2)var x = 11 + 2 + "a";    //13a
