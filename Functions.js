5d18c371c2df052797b2ac09457f268fd9f157b3
zhangr16 <zhangr16@uni.coventry.ac.uk>
2016-03-08 23:53:51 +0000

    Create Functions.js

---
+++ Functions.js
@@ -0,0 +1,12 @@
+function name(parameter1, parameter2, parameter3) {
+    code to be executed;
+}
+
+
+eg: 
+<script>
+function andy(f) {
+    return (5+1) * (f-2);
+}
+document.getElementById("demo").innerHTML = andy(3);
+</script>
