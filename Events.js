cea2551c6beebbe04227ac4fecebbd335ef4e93f
zhangr16 <zhangr16@uni.coventry.ac.uk>
2016-04-12 20:33:38 +0100

    Create Events.js

---
+++ Events.js
@@ -0,0 +1,13 @@
+HTML Events(eg):
+(1)An HTML web page has finished loading
+(2)An HTML input field was changed
+(3)An HTML button was clicked
+
+
+eg:
+<script>
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
+</script>
+
